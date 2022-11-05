223a8a499af2dd24ee4c4cf5b5e1abb47b12d4cb
Emmanuel-Aggrey <aggrey.en@gmail.com>
2022-11-05 03:31:43 +0000

    save reqiest image to file

--- HRMSPROJECT/settings.py
+++ HRMSPROJECT/settings.py
@@ -112,26 +112,26 @@ WSGI_APPLICATION = 'HRMSPROJECT.wsgi.application'
 
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('POSTGRES_DB'),
-        'USER': config('POSTGRES_USER'),
-        'PASSWORD': config('POSTGRES_PASSWORD'),
-        'HOST': config('POSTGRESS_HOST'),
-        'PORT': config('POSTGRESS_PORT'),
-    }
-}
-
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('POSTGRES_DB'),
+#         'USER': config('POSTGRES_USER'),
+#         'PASSWORD': config('POSTGRES_PASSWORD'),
+#         'HOST': config('POSTGRESS_HOST'),
+#         'PORT': config('POSTGRESS_PORT'),
 #     }
 # }
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 
--- hrms/hr_views.py
+++ hrms/hr_views.py
@@ -87,9 +87,8 @@ def hr_reports(request, data_value=None):
     emp_exceed_leave = qs.filter(leave_employees__from_leave=False, leave_employees__end__lt=timezone.now()).values(
         'leave_employees__employee__first_name', 'leave_employees__employee__last_name', 'leave_employees__end')
 
-    emp_on_leave = qs.filter(leave_employees__from_leave=False).values('leave_employees__employee__employee_id','leave_employees__from_leave', 'leave_employees__hr_manager', 'leave_employees__line_manager',
+    emp_on_leave = qs.filter(leave_employees__from_leave=False).values('leave_employees__employee__employee_id', 'leave_employees__from_leave', 'leave_employees__hr_manager', 'leave_employees__line_manager',
                                                                        'leave_employees__employee__first_name', 'leave_employees__employee__last_name', 'leave_employees__pk', 'leave_employees__end')
-
 
     emp_from_leave_recent = qs.filter(leave_employees__from_leave=True).values(
         'leave_employees__employee__first_name', 'leave_employees__employee__last_name', 'leave_employees__end').reverse()[:5]
@@ -99,7 +98,6 @@ def hr_reports(request, data_value=None):
 
     turn_over_rate = qs.values(
         'status', 'date_employed__year').annotate(employee_count=Count('date_employed'))
-
 
     # print(turn_over_rate)
 
@@ -129,7 +127,7 @@ def hr_reports(request, data_value=None):
         'leave_this_year': leave_this_year,
         'leave_applications': leave_applications,
         'emp_exceed_leave': emp_exceed_leave,
-        
+
         'emp_on_leave': emp_on_leave,
         'emp_from_leave_recent': emp_from_leave_recent,
         'employment_rate': employment_rate,
@@ -216,27 +214,23 @@ def time_attendance(request):
 
         request.session['date_from'] = date_from
         request.session['date_to'] = date_to
-        request.session['DATETIME'] ="DATETIME"
-       
+        request.session['DATETIME'] = "DATETIME"
+
         print(date_from, date_to)
-# 
+#
         # print(date_from, date_to, startTime, endTime)
-        
+
         sql = "SELECT  [DeptName] AS Department, 'In' as [StatusText In], Count(case StatusText when 'In' then 1 end) as Count_In, 'Out' as [Status_out],Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATETIME) BETWEEN '{}' AND '{}' AND DeptName IS NOT NULL GROUP BY [DeptName] ORDER BY [DeptName]".format(
             date_from, date_to)
-        
 
     elif date_to:
-        request.session['date_from'] = date_from 
-        request.session['date_to'] = date_to 
-        request.session['DATETIME'] ="DATE"
+        request.session['date_from'] = date_from
+        request.session['date_to'] = date_to
+        request.session['DATETIME'] = "DATE"
         # print(date_from, date_to)
-    
 
-        sql = "SELECT  [DeptName] AS Department, 'In' as [StatusText In], Count(case StatusText when 'In' then 1 end) as Count_In, 'Out' as [Status_out],Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATE) BETWEEN '{}' AND '{}' AND DeptName IS NOT NULL  GROUP BY [DeptName] ORDER BY [DeptName]".format(date_from, date_to)
-
-    
-    
+        sql = "SELECT  [DeptName] AS Department, 'In' as [StatusText In], Count(case StatusText when 'In' then 1 end) as Count_In, 'Out' as [Status_out],Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATE) BETWEEN '{}' AND '{}' AND DeptName IS NOT NULL  GROUP BY [DeptName] ORDER BY [DeptName]".format(
+            date_from, date_to)
 
     if sql_server.server_not_connected:
         print('server not connected')
@@ -259,23 +253,21 @@ def get_department(request, department):
     date_to = request.session.get('date_to')
     DATETIME = request.session.get('DATETIME')
 
-
     sql = "SELECT  [Name] , 'In' as [StatusText In], Count(case StatusText when 'In' then 1 end) as Count_In, 'Out' as [Status_out],Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS {}) BETWEEN '{}' AND '{}' AND [DeptName]='{}' GROUP BY [Name] ORDER BY [Name]".format(
-            DATETIME,date_from, date_to,department)
+        DATETIME, date_from, date_to, department)
 
     cursor = sql_server.cursor.execute(sql)
     rows = cursor.fetchall()
     columns = [column[0] for column in cursor.description]
     result = [dict(zip(columns, row)) for row in rows]
 
-
-
     # print(sql)
     # print(date_from, date_to)
     # print(result)
     return Response(result)
 
-@group_required(['HR','FO'])
+
+@group_required(['HR', 'FO'])
 @api_view(['GET'])
 def clockins(request):
 
@@ -284,22 +276,21 @@ def clockins(request):
     today_ = today.strftime('%Y-%m-%d')
     yesterday = today - timedelta(days=1)
     yesterday = yesterday.strftime('%Y-%m-%d')
-    week = today- timedelta(days=7)
-    week = week.strftime('%Y-%m-%d')    
+    week = today - timedelta(days=7)
+    week = week.strftime('%Y-%m-%d')
 
     if sql_server.server_not_connected:
         print('server not connected')
         # return Response()
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-
         # cursor =sql_server.connection.cursor()
     else:
         sql_today = "SELECT  [StatusText] ,  Count(case StatusText when 'In' then 1 end) as Count_In, Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATE) ='{}' AND DeptName IS NOT NULL GROUP BY [StatusText] ORDER BY [StatusText]".format(today_)
         sql_yesterday = "SELECT  [StatusText] ,  Count(case StatusText when 'In' then 1 end) as Count_In, Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATE) ='{}' AND DeptName IS NOT NULL GROUP BY [StatusText] ORDER BY [StatusText]".format(yesterday)
-        sql_week = "SELECT  [StatusText] ,  Count(case StatusText when 'In' then 1 end) as Count_In, Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATE) BETWEEN '{}' AND '{}' AND DeptName IS NOT NULL GROUP BY [StatusText] ORDER BY [StatusText]".format(week,today_)
-        sql_department_yesterday = "SELECT  [DeptName] AS Department,  Count(case StatusText when 'In' then 1 end) as Count_In, Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATE) = '{}' AND DeptName IS NOT NULL GROUP BY [DeptName] ORDER BY [DeptName]".format(yesterday)
-
+        sql_week = "SELECT  [StatusText] ,  Count(case StatusText when 'In' then 1 end) as Count_In, Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATE) BETWEEN '{}' AND '{}' AND DeptName IS NOT NULL GROUP BY [StatusText] ORDER BY [StatusText]".format(week, today_)
+        sql_department_yesterday = "SELECT  [DeptName] AS Department,  Count(case StatusText when 'In' then 1 end) as Count_In, Count(case StatusText when 'Out' then 1 end) as Count_Out FROM [dbo].[V_Record] WHERE [StatusText] in ('In', 'Out') AND CAST(CheckTime AS DATE) = '{}' AND DeptName IS NOT NULL GROUP BY [DeptName] ORDER BY [DeptName]".format(
+            yesterday)
 
         cursor = sql_server.cursor.execute(sql_today)
         rows = cursor.fetchall()
@@ -316,44 +307,70 @@ def clockins(request):
         columns = [column[0] for column in cursor.description]
         result_week = [dict(zip(columns, row)) for row in rows]
 
-
-
         cursor = sql_server.cursor.execute(sql_department_yesterday)
         rows = cursor.fetchall()
         columns = [column[0] for column in cursor.description]
         result_department_yesterday = [dict(zip(columns, row)) for row in rows]
 
         data = {
-            'sql_today':result_today,
-            'sql_yesterday':result_yesterday,
+            'sql_today': result_today,
+            'sql_yesterday': result_yesterday,
             'sql_week': result_week,
             'result_department_yesterday': result_department_yesterday,
 
         }
-       
+
         return Response(data)
 
 
 @api_view(['POST'])
 def update_anviz_user(request):
-#     UPDATE [anviz].[dbo].[Userinfo]
-# SET Picture = 
-#     (SELECT  BulkColumn FROM OPENROWSET(BULK  N'C:\783be074781f55bbe26bdefa33f9b1fc.jpg', SINGLE_BLOB) AS x)
-# WHERE Userid =184
+    #     UPDATE [anviz].[dbo].[Userinfo]
+    # SET Picture =
+    #     (SELECT  BulkColumn FROM OPENROWSET(BULK  N'C:\783be074781f55bbe26bdefa33f9b1fc.jpg', SINGLE_BLOB) AS x)
+    # WHERE Userid =184
     # image =  request.data.get('image')
+
+    from PIL import Image
+    import os
+
+    image_path = "../profile"
+
+    # os.mkdir(image_path)
 
     if request.method == 'POST':
         anviz_user = request.data.get('anviz_user')
-        image= request.FILES.get('image')
+        profile = request.FILES.get('image')
 
 
-        print(anviz_user,image)
 
-        sql = "UPDATE [anviz].[dbo].[Userinfo] SET Picture =(SELECT  BulkColumn FROM OPENROWSET(BULK  N'{}', SINGLE_BLOB) AS Picture) WHERE Userid ='{}'".format(image,anviz_user)
+        image_path = "/media/aggrey/1EF5-7DBA/HR/profile"
+     
+        img=  Image.open(profile)
+        size = 128, 128
+        img.thumbnail(size)
+        image = img.save(f"{image_path}/{profile}")
 
-        cursor = sql_server.cursor.execute(sql)
+        old_path = f'{image_path}/{profile}'
+        new_path = f'{image_path}/{anviz_user}.jpg'
+
+        os.rename(old_path, new_path)
+
+        img.show(new_path)
+
+
+        
+
+
+
+
+
+        print(anviz_user, profile)
+
+        # sql = "UPDATE [anviz].[dbo].[Userinfo] SET Picture =(SELECT  BulkColumn FROM OPENROWSET(BULK  N'{}', SINGLE_BLOB) AS Picture) WHERE Userid ='{}'".format(image,anviz_user)
+
+        # cursor = sql_server.cursor.execute(sql)
 
         # print(cursor)
 
         return Response(anviz_user)
-     
