593561103d6909c2180097553d4c82dbfd385b60
Emmanuel-Aggrey <aggrey.en@gmail.com>
2022-11-20 01:52:19 +0000

    .

--- HRMSPROJECT/sql_server.py
+++ HRMSPROJECT/sql_server.py
@@ -9,17 +9,17 @@ DATABASE = config('DATABASE')
 USER = config('USER_ID')
 PASSWORD = config('PASSWORD')
 conntected_to_server = True
-# try:
-#     connection = pyodbc.connect(
-#         "DRIVER={ODBC Driver 17 for SQL Server};" + f"SERVER={SERVER};"
-#         f"DATABASE={DATABASE};"
-#         f"UID={USER};"
-#         f"PWD={PASSWORD};")
-#     cursor = connection.cursor()
-# except:
-#     server_not_connected = 'Server not connected'
-#     conntected_to_server = False
-#     print('server_not_connected',SERVER)
+try:
+    connection = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};" + f"SERVER={SERVER};"
+        f"DATABASE={DATABASE};"
+        f"UID={USER};"
+        f"PWD={PASSWORD};")
+    cursor = connection.cursor()
+except:
+    server_not_connected = 'Server not connected'
+    conntected_to_server = False
+    print('server_not_connected',SERVER)
 
 
 
